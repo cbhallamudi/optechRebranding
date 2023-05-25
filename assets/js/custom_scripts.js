428bab8b1d7fd899fcc637e32dded7cb4bf473ba
cbhallamudi <cbhallamudi@gmail.com>
2023-05-25 12:26:54 +0530

    Custom Javascript added to Portfolio

--- assets/js/custom_scripts.js
+++ assets/js/custom_scripts.js
@@ -79,3 +79,49 @@ function slideLeft() {
         buttons[0].classList.remove('inactive');
     }
 };
+
+
+
+$(document).ready(function() {
+    var portfolioSlidesCount = $(".swiper-slide").length;
+    var midSlide = Math.round(portfolioSlidesCount/2);
+  // Assign some jquery elements we'll need
+  var $swiper = $(".swiper-container");
+  var $bottomSlide = null; // Slide whose content gets 'extracted' and placed
+  // into a fixed position for animation purposes
+  var $bottomSlideContent = null; // Slide content that gets passed between the
+  // panning slide stack and the position 'behind'
+  // the stack, needed for correct animation style
+
+  var mySwiper = new Swiper(".swiper-container", {
+    slidesPerView: 3,
+    centeredSlides: true,
+    roundLengths: true,
+    loop: true,
+    loopedSlides: 5,
+    spaceBetween: 10,
+    loopAdditionalSlides: 30,
+    autoplay: {
+        delay: 100000000000000,
+    },
+    pagination: {                       //pagination(dots)
+       el: '.swiper-pagination',
+       clickable: true
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    },
+    breakpoints: {
+
+        767: {
+            slidesPerView:3,
+            spaceBetween: 30,
+        },
+        574: {
+            slidesPerView:1,
+            spaceBetween: 10,
+        }
+    }
+  });
+});
